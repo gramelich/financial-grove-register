348d7c449a395b9d038cf9a54f0bb8f779c31982
gramelich <68996556+gramelich@users.noreply.github.com>
2024-11-28 13:12:47 -0300

    Update NotificationSettings.tsx

--- src/components/settings/NotificationSettings.tsx
+++ src/components/settings/NotificationSettings.tsx
@@ -1,7 +1,7 @@
 import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Control } from "react-hook-form";
+import { Control, useForm, Controller } from "react-hook-form";
 import { SettingsFormValues } from "./SettingsForm";
 import { Checkbox } from "@/components/ui/checkbox";
 
