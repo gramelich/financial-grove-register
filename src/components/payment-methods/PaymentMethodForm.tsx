6bcbaf405a5ab000748149bcbcdd2f6f80287cdc
gramelich <68996556+gramelich@users.noreply.github.com>
2024-11-28 10:39:25 -0300

    Update PaymentMethodForm.tsx

--- src/components/payment-methods/PaymentMethodForm.tsx
+++ src/components/payment-methods/PaymentMethodForm.tsx
@@ -12,7 +12,7 @@ import { Plus } from "lucide-react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
 import { toast } from "sonner";
-import { PaymentMethodForm } from "@/components/payment-methods/PaymentMethodForm";
+
 
 const FormasDePagamento = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
